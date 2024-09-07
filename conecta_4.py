78af3cfa2e631920a807bc88e25537abee840a10
Antonella <antomuttib@gmail.com>
2024-09-07 13:19:57 -0300

    actualiza método encargado de ejecutar el juego

--- conecta_4.py
+++ conecta_4.py
@@ -2,11 +2,8 @@ from os import system
 import platform
 class Game:
     def __init__(self) -> None:
-        self.n_row = 6
-        self.n_col = 7
-        self.disc = 1
-        self.disc_lacated = 0
-        self.board = [[0 for _ in range(self.n_col)] for _ in range(self.n_row)] 
+        self.disc = 'A'
+        self.board = [['O' for _ in range(7)] for _ in range(6)] 
 
     def clear_screen(self):
         op_sys = platform.system()
@@ -23,18 +20,19 @@ class Game:
                 print(cell, end="  ")
             print(end="\n")
 
-    def shift(self):
-        if self.disc == 1:
-            self. disc = 2
+    def shift_disc(self):
+        if self.disc == 'A':
+            self.disc = 'B'
         else:
-            self.disc = 1
+            self.disc = 'A'
 
     def check_winner(self, row, col, disc):
+        # Raliza una búsqueda de patrones en una matriz bidimensional
     
         # Define las direcciones en las que se verificará (horizontal, vertical, diagonal)
-        directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
+        axis = [(0, 1), (1, 0), (1, 1), (1, -1)]
         
-        for d in directions:
+        for d in axis:
             count = 1
             
             # Verifica en ambas direcciones (+d y -d)
@@ -48,56 +46,48 @@ class Game:
                 y = col + dy
                 
                 # Controla que la celda a verificar no esté fuera de rango (0 a 6 en el eje x, 0 a 5 el eje y)
-                # y que el valor de la celda sea el mismo que el disco introducido
+                # y que el valor de la celda sea el mismo que el disco introducido.
                 while 0 <= x < len(self.board) and 0 <= y < len(self.board[0]) and self.board[x][y] == disc:
                     count += 1
                     if count == 4:
                         print('ganador jugador', disc)
                         return True
                     
-                    # Continua revisando las celdas en la dirección en la que encontró el siguiente disco
-                    # hasta que encuentre 4 discos iguales o encuentre una celda vacía
+                    # Continua revisando las celdas en el mismo eje hasta que encuentre 4 discos 
+                    # iguales consecutivos o encuentre una celda vacía.
                     x += dx
                     y += dy
         print('no hay ganador')            
         return False
 
+    def play(self):
+        playing = True
+        row = 5
+        disc_in_game = False
+        while playing:
+            # Controla si ya se introdujo el disco
+            if not disc_in_game:
+                col= int(input('columna: '))
+            disc_in_game = True
 
-    def check_full_col(self, input: int):
-        if self.board[0][input] == 1 or self.board[0][input] == 2:
-            self.n_row = 6
-            self.disc_lacated -=1
-            return False
+            if row >= 0 and self.board[row][col] == 'O':
+                self.board[row][col]=self.disc
+                self.show_board()
+                ganador = self.check_winner(row, col, self.disc)
+                if ganador:
+                    playing = False
+                self.shift_disc()
+                row = 5
+                disc_in_game = False
+            elif row < 0:
+                row = 5
+                disc_in_game = False
+            elif not self.board[row][col] == 'O':
+                row -= 1 
+                
+
         return True
-    
-    def play(self, input: int):
-
-        self.n_row -= 1        
-        if not self.check_full_col(input):
-            
-            return
-        if self.board[self.n_row][input] == 0:
-            self.board[self.n_row][input] = self.disc
-            self.check_winner(self.n_row, input, self.disc)
-            self.show_board()
-            self.n_row = 6
-            return True
-        elif self.n_row == 0:
-                self.n_row=6
-                return
-            
-        self.play(input)
-
-    def start(self):
-        while True:
-            pos = int(input('col: '))
-            self.play(pos)
-            self.disc_lacated +=1
-            self.shift()
-
-            if self.disc_lacated == 42:
-                break
 
 if __name__ == '__main__':
     game = Game()
-    game.start()
+    game.play()
