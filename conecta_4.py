635ddff76af7aa90ed38d0c8079e15af1184bb50
Antonella <antomuttib@gmail.com>
2024-09-18 12:18:08 -0300

    añade pruebas adicionales para mejorar el rendimiento del bot

--- conecta_4.py
+++ conecta_4.py
@@ -1,74 +1,123 @@
-from random import randint
-JUGADOR = 'A'
-BOT  = 'B'
-VACIO = ' '
+import numpy as np
+HUMAN = 1
+BOT  = 2
+EMPTY = 0
+COLS = 7
+ROWS = 6
+FOUR_CONNECTED = 4
 def find_lower_position(state, col):
     row = 5
     while row >= 0:
-        if state[row][col] == VACIO:
+        if state[row][col] == EMPTY:
             return row
         row -= 1
     return None
 
 def possible_new_states(state, is_maximizing):
     children = []
-    player = BOT if is_maximizing else JUGADOR
+    player = BOT if is_maximizing else HUMAN
 
     for col in range(7):
         row = find_lower_position(state, col)
         if row is not None: 
-            new_state = [row[:] for row in state]
+            new_state = state.copy()
             new_state[row][col]=player
             children.append(new_state)
     return children
 
+def check_four_connected(state, player):
+    # Comprobar horizontal
+    for c in range(COLS-3):
+        for r in range(ROWS):
+            if all([state[r][c+i] == player for i in range(FOUR_CONNECTED)]):
+                return True
+
+    # Comprobar vertical
+    for c in range(COLS):
+        for r in range(ROWS-3):
+            if all([state[r+i][c] == player for i in range(FOUR_CONNECTED)]):
+                return True
+
+    # Comprobar diagonal (positiva)
+    for c in range(COLS-3):
+        for r in range(ROWS-3):
+            if all([state[r+i][c+i] == player for i in range(FOUR_CONNECTED)]):
+                return True
+
+    # Comprobar diagonal (negativa)
+    for c in range(COLS-3):
+        for r in range(3, ROWS):
+            if all([state[r-i][c+i] == player for i in range(FOUR_CONNECTED)]):
+                return True
+
+    return False
+
+def check_board(state, player):
+    score = 0
+
+    # Puntuación horizontal
+    for r in range(ROWS):
+        row_array = [int(i) for i in list(state[r, :])]
+        for c in range(COLS-3):
+            window = row_array[c:c+FOUR_CONNECTED]
+            score += check_window(window, player)
+
+    # Puntuación vertical
+    for c in range(COLS):
+        columna_array = [int(i) for i in list(state[:, c])]
+        for r in range(ROWS-3):
+            window = columna_array[r:r+FOUR_CONNECTED]
+            score += check_window(window, player)
+
+    # Puntuación diagonal positiva
+    for r in range(ROWS-3):
+        for c in range(COLS-3):
+            window = [state[r+i][c+i] for i in range(FOUR_CONNECTED)]
+            score += check_window(window, player)
+
+    # Puntuación diagonal negativa
+    for r in range(3, ROWS):
+        for c in range(COLS-3):
+            window = [state[r-i][c+i] for i in range(FOUR_CONNECTED)]
+            score += check_window(window, player)
+
+    return score
+
+
+def check_window(window, player):
+    # Está evaluando es una "ventana" de 4 celdas consecutivas en el tablero, 
+    # tanto horizontal, vertical como diagonalmente. 
+    # ("window" es una subsección de 4 espacios adyacentes en el tablero)
+    score = 0
+    oponente = HUMAN if player == BOT else BOT
+
+    if window.count(player) == 4:
+        score += 100
+    elif window.count(player) == 3 and window.count(EMPTY) == 1:
+        score += 5
+    elif window.count(player) == 2 and window.count(EMPTY) == 2:
+        score += 2
+
+    if window.count(oponente) == 3 and window.count(EMPTY) == 1:
+        score -= 4
+
+    return score
 
 def evaluate(state):
-    if all(cell != ' ' for row in state for cell in row):
-        return 0            
-    for row in range(6):
-        for col in range(7):
-            axis = [(0, 1), (1, 0), (1, 1), (1, -1)]
-            if state[row][col] != VACIO:
-                if state[row][col] == BOT:
-                    for d in axis:
-                        count = 1
-                        
-                        for dir in [1, -1]:
+    score = 0
+    for row in range(ROWS):
+        for col in range(COLS):
+            if state[row][col] == HUMAN:
+                score -= check_board(state, HUMAN)
+            elif state[row][col] == BOT:
+                score += check_board(state, BOT)
+    if check_four_connected(state, BOT):
 
-                            dx = d[0] * dir
-                            dy = d[1] * dir
+        return 1
+    elif check_four_connected(state, HUMAN):
 
-                            x = row + dx
-                            y = col + dy
-                            while 0 <= x < len(state) and 0 <= y < len(state[0]) and state[x][y] == BOT:                        
-                                count += 1
-                                if count == 4:
-                                    return 1
-
-                                x += dx
-                                y += dy
-                elif state[row][col] == JUGADOR:
-
-                    for d in axis:
-                        count = 1
-                        
-                        for dir in [1, -1]:
-
-                            dx = d[0] * dir
-                            dy = d[1] * dir
-
-                            x = row + dx
-                            y = col + dy
-                            while 0 <= x < len(state) and 0 <= y < len(state[0]) and state[x][y] == JUGADOR:
-                                count += 1
-                                if count == 4:
-
-                                    return -1
-                                
-                                x += dx
-                                y += dy
-    return 0
+        return -1
+    return score
 
 def minimax(state, is_maximizing, alpha, beta, depth = 4):
 
@@ -115,15 +164,7 @@ def best_move(state):
 
 
 def play():
-    board = [
-                [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                [' ', ' ', ' ', ' ', ' ', ' ', ' ']
-            ]
-
+    board = np.zeros((ROWS, COLS))
 
     playing = True
     while playing:
@@ -132,7 +173,7 @@ def play():
 
 
         row = find_lower_position(board, col)
-        board[row][col]=JUGADOR
+        board[row][col]=HUMAN
         for line in board:
             print(line)
         print()
